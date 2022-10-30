2ebb4e8e3a3e3cb1ac2b66592d59d78ef98d0aa7
Connery <1514845104@qq.com>
2022-10-30 20:46:07 +0800

    1

--- src/main/java/com/example/dorm/DormApplication.java
+++ src/main/java/com/example/dorm/DormApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DormApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DormApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DormApplication.class, args);
+    }
 
 }
