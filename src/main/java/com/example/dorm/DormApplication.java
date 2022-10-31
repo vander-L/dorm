b6aa36c5a150d6ba567ee3188f3ade4d279b29ac
Connery <1514845104@qq.com>
2022-10-31 19:03:31 +0800

    1

--- src/main/java/com/example/dorm/DormApplication.java
+++ src/main/java/com/example/dorm/DormApplication.java
@@ -2,8 +2,10 @@ package com.example.dorm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class DormApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/dorm/config/MySpringMvcConfig.java
@@ -0,0 +1,15 @@
+package com.example.dorm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MySpringMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/index").setViewName("index");
+    }
+}
