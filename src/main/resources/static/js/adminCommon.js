9d129306ac1d8efbc4aa97b851f1f100e0d3ec69
Connery <1514845104@qq.com>
2022-11-26 17:52:46 +0800

    1

--- src/main/resources/static/js/adminCommon.js
+++ src/main/resources/static/js/adminCommon.js
@@ -1,14 +1,22 @@
+let adminName = document.getElementById("adminName")
+let adminId = document.getElementById("adminId")
+let adminTel = document.getElementById("adminTel")
+
 $.ajax({
     url: '/adminData',
     async: false,
     dataType: 'json',
     type: 'get',
     success: function (data){
-        $("#adminName").innerHTML = data.name
-        $("#adminId").innerHTML = data.id
-        $("#adminTel").innerHTML = data.phone
+        adminName.innerHTML = data.name
+        adminId.innerHTML = data.id
+        adminTel.innerHTML = data.phone
     }
 })
+
+console.log(adminName.innerHTML)
+console.log(adminId.innerHTML)
+console.log(adminTel.innerHTML)
 
 let logout = function (){
     alert("注销成功！")
